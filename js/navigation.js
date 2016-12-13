92af7b7fafbc6e494e1bf7512ac01972183af4f0
matthew william chamberlain <845862784@kent-202-15m.local>
2016-12-14 11:23:54 +1300

    Initial commit

--- js/navigation.js
+++
@@ -1,7 +0,0 @@
-$('nav ul').toggle(0);
-$('nav button').click(function(){
-
-	// Toggle the visibility of the nav links
-	$('nav ul').toggle(400);
-
-});
--- js/patterns.js
+++
@@ -1,5 +0,0 @@
-//Regular expressions for validation.js
-
-var namePattern = new RegExp("^[a-zA-Z.'-]{2,30}$");
-var emailPattern = new RegExp("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?");
-var postPattern = new RegExp("^[a-zA-Z0-9!#$%&*.'-]{2,30000}$");
